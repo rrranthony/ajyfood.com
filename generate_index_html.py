6018ac817a2982baf2b555fce30689fdb84e89b2
Anthony Young <rrranthony@users.noreply.github.com>
2024-08-21 15:22:50 -0700

    Add script to generate index.html

---
+++ generate_index_html.py
@@ -0,0 +1,45 @@
+import os
+
+
+def get_recipe_title(file_path):
+    basename = os.path.basename(file_path)
+    basename_without_extension = os.path.splitext(basename)[0]
+    title = " ".join(word.capitalize() for word in basename_without_extension.split("-"))
+    return title
+
+def get_recipe_filenames(recipe_dir):
+    files = os.listdir(recipe_dir)
+    recipe_filenames = [
+        f for f
+        in files
+        if (not f.startswith(".") and len(f) > 1)
+    ]
+    return recipe_filenames
+
+def generate_index_html(recipe_filenames):
+    href_base = "https://ajyfood.com/recipes"
+    links = [
+        f'<li><a href="{href_base}/{fn}">{get_recipe_title(fn)}</a></li>' for fn
+        in sorted(recipe_filenames)
+    ]
+    html = "".join([
+        "<!DOCTYPE html>\n",
+        "<head><title>Recipes</title></head>\n",
+        "<body>\n",
+        "<h1>Recipes</h1>\n",
+        "<ul>\n",
+          "\n".join(links),
+        "</ul>\n",
+        "</body>\n",
+        "</html>\n",
+    ])
+    return html
+
+
+if __name__ == "__main__":
+    recipe_dir = "recipes"
+    recipe_filenames = get_recipe_filenames(recipe_dir)
+    index_html = generate_index_html(recipe_filenames)
+    print(f"index.html generated:\n\n{index_html}")
+    with open("index.html", "w") as f:
+        f.write(index_html)
